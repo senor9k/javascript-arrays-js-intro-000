8e789ab28b617d3ed6808e7da4c17b5c37bbadf4
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-04 22:07:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,6 @@ function addElementToBeginningOfArray(array, element){
   var newArray = [element, ...array]
   return newArray
 }
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
