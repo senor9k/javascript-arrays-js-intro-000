75c78d5cd20e6128dc864cecb8eab2f7f62cc8ee
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-04 22:10:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,7 @@ function addElementToEndOfArray(array, element){
 function destructivelyAddElementToEndOfArray(array, element){
   return array.push(element)
 }
+function accessElementInArray(array, index){
+  return array[index]
+}
+
