1b29ea6da9587c0b99a9e8a8fd845b07aa2c9f3e
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-04 22:27:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
-function destructivelyRemoveElementFromEndOfArray(){
+function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
   return array
 }
